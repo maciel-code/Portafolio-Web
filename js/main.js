70b8274ba48ca1d4e1d9b53a0fc3308de1a6b2e3
Brandon MAciel <brandonmaciel96@gmail.com>
2021-06-24 18:53:29 -0300

    primer compromiso

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function mensaje(){
+    document.getElementById("enviado").innerHTML="Mensaje Eviado !!! Puedes hablarme por whatsapp tambien 1134137575"
+}
+document.getElementById("boton").onclick=function(){
+    mensaje();
+}
